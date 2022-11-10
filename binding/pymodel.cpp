6de36c0cdf6af73b4bbb35657a356e764b55423c
larsbpf <lbpf@cs.aau.dk>
2022-11-10 10:04:00 +0100

    Support for pybinds for CFG

--- binding/pymodel.cpp
+++ binding/pymodel.cpp
@@ -1,5 +1,6 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <vector>
 
 #include "model/cfg.hpp"
 #include "model/controller.hpp"
@@ -63,5 +64,56 @@ void addModelModule(py::module& m){
         .def("assign", &MiniMC::Model::InstructionStream::operator=)
         .def("output",&MiniMC::Model::InstructionStream::output);
 
+    /* edge */
+    py::class_<MiniMC::Model::Edge,std::shared_ptr<MiniMC::Model::Edge>>(submodule, "Edge")
+        .def(py::init<MiniMC::Model::Location_ptr,MiniMC::Model::Location_ptr>())
+        .def("getInstructions", [](MiniMC::Model::Edge& self){
+            MiniMC::Model::InstructionStream is = self.getInstructions();
+            return is;
+        })
+        .def("getTo", &MiniMC::Model::Edge::getTo)
+        .def("getFrom",&MiniMC::Model::Edge::getFrom)
+        .def("setTo", &MiniMC::Model::Edge::setTo);
 
+    /* cfg */
+    py::class_<MiniMC::Model::CFA>(submodule, "CFA")
+        .def(py::init())
+        .def("makeLocation", &MiniMC::Model::CFA::makeLocation)
+        .def("makeEdge", &MiniMC::Model::CFA::makeEdge)
+        .def("getInitialLocation", &MiniMC::Model::CFA::getInitialLocation)
+        .def("deleteEdge", &MiniMC::Model::CFA::deleteEdge)
+        .def("deleteLocation", &MiniMC::Model::CFA::deleteLocation);
+
+    py::class_<MiniMC::Model::Function, std::shared_ptr<MiniMC::Model::Function>>(submodule, "Function")
+    .def("getSymbol", &MiniMC::Model::Function::getSymbol)
+    .def("getParameters", &MiniMC::Model::Function::getParameters)
+    .def("getRegisterDescr", &MiniMC::Model::Function::getRegisterDescr)
+    .def("getRegisterStackDescr", &MiniMC::Model::Function::getRegisterStackDescr)
+    .def("getID", &MiniMC::Model::Function::getID)
+    .def("getReturnType", &MiniMC::Model::Function::getReturnType)
+    .def("getPrgm", &MiniMC::Model::Function::getPrgm);
+
+    /* location */
+    py::class_<MiniMC::Model::Location, std::shared_ptr<MiniMC::Model::Location>>(submodule, "Location")
+        .def("addEdge", &MiniMC::Model::Location::addEdge)
+        .def("addIncomingEdge", &MiniMC::Model::Location::addIncomingEdge)
+        .def("getOutGoingEdges", [](MiniMC::Model::Location& self){
+            std::vector<MiniMC::Model::Edge*> edges;
+            std::for_each(self.ebegin(), self.eend(), [&edges](auto& e) { edges.push_back(e); });
+            return edges;
+        })
+        .def("getIncomingEdges", [](MiniMC::Model::Location& self){
+            std::vector<MiniMC::Model::Edge*> edges;
+            std::for_each(self.iebegin(), self.ieend(), [&edges](auto& e) { edges.push_back(e); });
+            return edges;
+        })
+        .def("getCFG", &MiniMC::Model::Location::getCFG);
+
+    /* variables */
+    py::class_<MiniMC::Model::Value> value(submodule, "Value");
+    value.def("type_t", &MiniMC::Model::Value::type_t)
+        .def("getType", &MiniMC::Model::Value::getType)
+        .def("setType", &MiniMC::Model::Value::setType);
+
+    py::class_<MiniMC::Model::Constant>(submodule, "Constant", value);
 }
